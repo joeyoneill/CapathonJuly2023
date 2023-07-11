cec4600c46a705311ec98c0d72d5aea7e6447e33
Chahal <kirpa.chahal@capgemini.com>
2023-07-10 23:30:40 -0400

    added template and calander

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,21 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
+
+    public IActionResult Pricing()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();

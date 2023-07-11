aa421affa4aa3c20d135d40bc3425bdb790c0e5f
Joey O'Neill <40486240+joeyoneill@users.noreply.github.com>
2023-07-11 13:41:00 -0500

    Profile Page

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -27,12 +27,48 @@ namespace CAPATHON.Controllers
         [Authorize]
         public IActionResult Profile()
         {
-            return View(new
-            {
-                Name = User.Identity.Name,
-                EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
-                ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value
-            });
+            Console.WriteLine();
+
+            // attr init
+            string name = string.Empty;
+            string nickname = string.Empty;
+            string userId = string.Empty;
+
+            // Cast User Claims to List
+            var user_identities = User.Claims.ToList();
+
+            // Iterate Through user's identities to get "name" and "nickname"
+            bool has_name = false;
+            bool has_nickname = false;
+            for (int i = 0; i < user_identities.Count; i++) {
+                if (user_identities[i].Type.ToString() == "nickname") {
+                    nickname = user_identities[i].Value.ToString();
+                    has_nickname = true;
+                }
+                else if (user_identities[i].Type == "name") {
+                    name = user_identities[i].Value.ToString();
+                    has_name = true;
+                }
+            }
+
+            // Get user ID
+            var provider_and_userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
+            try {
+                string[] parts = provider_and_userId.Split('|');
+                userId = parts[1];
+            }
+            catch {
+                return NotFound();
+            }
+
+            var ViewModel = new UserProfileViewModel {
+                UserId = userId,
+                Name = nickname,
+                Email = name
+            };
+
+            return View(ViewModel);
         }
 
         // ACTION: /Account/Logout
---
+++ Models/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAPATHON.Models.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public string? UserId { get; internal set; }
+        public string? Name { get; internal set; }
+        public string? Email { get; internal set; }
+    }
+}

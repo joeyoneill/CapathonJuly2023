89ce570bd163f29c7b2961c319be081b10de7666
Joey O'Neill <40486240+joeyoneill@users.noreply.github.com>
2023-07-12 16:59:50 -0500

    /Session/SelectBusiness

--- Models/Business.cs
+++ Models/Business.cs
@@ -7,7 +7,7 @@ namespace CAPATHON.Models
 {
     public class Business
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string? Name { get; set; }
     }
 }
